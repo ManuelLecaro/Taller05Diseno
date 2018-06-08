7bd59bc6c0b0ff118412537d2d425f71fef164e5
Jorge Paladines <joedpabo@gmail.com>
2018-06-07 21:31:45 -0500

    Solución de OCP

---
+++ TallerSolid/src/OCPsolution/Vehiculo.java
@@ -0,0 +1,12 @@
+package OCPsolution;
+
+import vehiculos.*;
+
+
+public interface Vehiculo {
+	
+    public void escuchar();
+    public void girarIzquierda();
+    public void girarDerecha();
+	
+}
---
+++ TallerSolid/src/OCPsolution/testeadorDeGiros.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OCPsolution;
+
+/**
+ *
+ * @author ASUS
+ */
+public class testeadorDeGiros {
+    public void girarIzquierda(Vehiculo vehiculo){
+        vehiculo.girarIzquierda();
+    }
+    public void girarDerecha(Vehiculo vehiculo){
+        vehiculo.girarDerecha();
+    }
+}
